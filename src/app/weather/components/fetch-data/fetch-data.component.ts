2e589c1b763ecf4847f3d883f672aa81bc311f9d
nakira974 <maxime.loukhal@gmail.com>
2023-05-02 01:26:40 +0200

    - Update front add search bar - Add cross origins to client's method in WeatherforecastController.java

--- src/app/weather/components/fetch-data/fetch-data.component.ts
+++ src/app/weather/components/fetch-data/fetch-data.component.ts
@@ -2,11 +2,15 @@ import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Chart, Plugin } from 'chart.js/auto';
 import { WeatherForecast } from '@core/models/weatherforecast';
+
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html',
 })
 export class FetchDataComponent {
+  public countryCode: string = '';
+  public regionCode: string = '';
+  public cityName: string = '';
   public forecasts: WeatherForecast[] = [];
 
   public chartAreaBorder: Plugin = {
@@ -28,81 +32,81 @@ export class FetchDataComponent {
     },
   };
 
-  constructor(http: HttpClient) {
-    http
-      .get<WeatherForecast[]>(
-        'https://monitor.lkh.coffee:8083/weather?city=Abbeville&country=US&state=LA'
-      )
-      .subscribe(
-        (result) => {
-          this.forecasts = result;
-          const weatherForecastChart = <HTMLCanvasElement>(
-            document.getElementById('forecasts')
-          );
-          const ctx = <CanvasRenderingContext2D>(
-            weatherForecastChart.getContext('2d')
-          );
-          Chart.register({
-            id: 'chartAreaBorder',
-          });
-          new Chart(ctx, {
-            type: 'line',
-            plugins: [this.chartAreaBorder],
-            options: {
-              animations: {
-                tension: {
-                  duration: 1000,
-                  easing: 'linear',
-                  from: 1,
-                  to: 0,
-                  loop: true,
-                },
-              },
-              scales: {
-                y: {
-                  // defining min and max so hiding the dataset does not change scale range
-                  min: 0,
-                  max: 135,
-                },
-              },
-              plugins: {
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                chartAreaBorder: {
-                  borderColor: 'red',
-                  borderWidth: 2,
-                  borderDash: [5, 5],
-                  borderDashOffset: 2,
-                },
-                tooltip: {
-                  callbacks: {
-                    label: function (context) {
-                      let label = context.dataset.label || '';
+  constructor(private http: HttpClient) {}
 
-                      if (label) {
-                        label += ': ';
-                      }
-                      if (context.parsed.y !== null) {
-                        label += `${((context.parsed.y - 32) * 5) / 9}°C`;
-                      }
-                      return label;
-                    },
+  getForecasts(countryCode: string, regionCode: string, cityName: string) {
+    const url = `https://monitor.lkh.coffee:8083/weather?country=${countryCode}&state=${regionCode}&city=${cityName}`;
+
+    this.http.get<WeatherForecast[]>(url).subscribe(
+      (result) => {
+        this.forecasts = result;
+        const weatherForecastChart = <HTMLCanvasElement>(
+          document.getElementById('forecasts')
+        );
+        const ctx = <CanvasRenderingContext2D>(
+          weatherForecastChart.getContext('2d')
+        );
+        Chart.register({
+          id: 'chartAreaBorder',
+        });
+        new Chart(ctx, {
+          type: 'line',
+          plugins: [this.chartAreaBorder],
+          options: {
+            animations: {
+              tension: {
+                duration: 1000,
+                easing: 'linear',
+                from: 1,
+                to: 0,
+                loop: true,
+              },
+            },
+            scales: {
+              y: {
+                // defining min and max so hiding the dataset does not change scale range
+                min: 0,
+                max: 135,
+              },
+            },
+            plugins: {
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              chartAreaBorder: {
+                borderColor: 'red',
+                borderWidth: 2,
+                borderDash: [5, 5],
+                borderDashOffset: 2,
+              },
+              tooltip: {
+                callbacks: {
+                  label: function (context) {
+                    let label = context.dataset.label || '';
+
+                    if (label) {
+                      label += ': ';
+                    }
+                    if (context.parsed.y !== null) {
+                      label += `${((context.parsed.y - 32) * 5) / 9}°C`;
+                    }
+                    return label;
                   },
                 },
               },
             },
-            data: {
-              labels: this.forecasts.map((row) => row.date),
-              datasets: [
-                {
-                  label: 'Évolution de la température',
-                  data: this.forecasts.map((row) => row.temperatureF),
-                },
-              ],
-            },
-          });
-        },
-        (error) => console.error(error)
-      );
+          },
+          data: {
+            labels: this.forecasts.map((row) => row.date),
+            datasets: [
+              {
+                label: 'Évolution de la température',
+                data: this.forecasts.map((row) => row.temperatureF),
+              },
+            ],
+          },
+        });
+      },
+      (error) => console.error(error)
+    );
   }
 }
